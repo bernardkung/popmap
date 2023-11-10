663dd2573cb7c648d0180f67a9ffe696d2fd5262
bernard kung <speak.freely@gmail.com>
2023-11-10 01:00:39 -0500

    data loading and passing to choropleth

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import './App.css'
 import * as d3 from "d3"
 import BarChart from './components/BarChart'
+import Choropleth from './components/Choropleth'
 import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [dataset, setDataset] = useState()
+  const [geodata, setGeodata] = useState()
+  const [popdata, setPopdata] = useState()
   const [loading, setLoading] = useState(true)
 
   // On Load
   useEffect(()=>{
     Promise.all([
-      d3.csv(
-        "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv"
-      )
+      d3.json("./data/counties-albers-10m.json"),
+      d3.csv("./data/co-est2022-alldata.csv", (c)=>{
+        // console.log(c)
+        return(c)
+      }),
     ])
-    .then((dataset)=>{
-      setDataset(dataset[0])
+    .then(([ geojson, popcsv ]) => {
+      setGeodata(geojson)
+      setPopdata(popcsv)
       setLoading(false)
     })
     return () => undefined
   }, [])
 
-  useEffect(()=>{
-    console.log("on state, data", dataset)
-    console.log("on state, loading", loading)
-  }, [dataset])
+  // // Check that data loaded
+  // useEffect(()=>{
+  //   console.log("on state, loading", loading)
+  //   console.log("on state, geodata", geodata)
+  //   console.log("on state, popdata", popdata)
+  // }, [geodata, popdata])
 
   return (
     <div class="container">
       { loading && <div>loading</div> }
-      { !loading && <BarChart dataset={dataset} loading={loading} /> }
+      { !loading && <Choropleth geodata={geodata} popdata={popdata} loading={loading}/> }
     </div>
   )
 }
--- src/components/Choropleth.js
+++ src/components/Choropleth.js
@@ -1,10 +1,36 @@
+import * as d3 from "d3";
+import * as topojson from "topojson-client"
+import { useEffect, useRef } from "react";
 
-chart = {
+
+const Choropleth = ({ geodata, popdata, loading }) => {
+  const ref = useRef()
+  const us = geodata
+
+  // // Check that data was passed correctly
+  // console.log(" chart, loading", loading)
+  // console.log(" chart, geodata", geodata)
+  // console.log(" chart, popdata", popdata)
+
+
+  useEffect(()=>{
+    // const svg = d3.create("svg")
+    //   .attr("width", 975)
+    //   .attr("height", 610)
+    //   .attr("viewBox", [0, 0, 975, 610])
+    //   .attr("style", "max-width: 100%; height: auto;");
+
+    const svg = d3.select(ref.current)
+      .attr("width", 975)
+      .attr("height", 610)
+      .attr("viewBox", [0, 0, 975, 610])
+      .attr("style", "max-width: 100%; height: auto;");
+
     const color = d3.scaleQuantize([1, 10], d3.schemeBlues[9]);
     const path = d3.geoPath();
     const format = d => `${d}%`;
-    const valuemap = new Map(data.map(d => [d.id, d.rate]));
-  
+    const valuemap = new Map(popdata.map(p => [p.STATE + p.COUNTY, p.POPESTIMATE2022]));
+
     // The counties feature collection is all U.S. counties, each with a
     // five-digit FIPS identifier. The statemap lets us lookup the name of 
     // the state that contains a given county; a state’s two-digit identifier
@@ -12,24 +38,18 @@ chart = {
     const counties = topojson.feature(us, us.objects.counties);
     const states = topojson.feature(us, us.objects.states);
     const statemap = new Map(states.features.map(d => [d.id, d]));
-  
+
     // The statemesh is just the internal borders between states, i.e.,
     // everything but the coastlines and country borders. This avoids an
     // additional stroke on the perimeter of the map, which would otherwise
     // mask intricate features such as islands and inlets. (Try removing
     // the last argument to topojson.mesh below to see the effect.)
     const statemesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
-  
-    const svg = d3.create("svg")
-        .attr("width", 975)
-        .attr("height", 610)
-        .attr("viewBox", [0, 0, 975, 610])
-        .attr("style", "max-width: 100%; height: auto;");
-  
-    svg.append("g")
-        .attr("transform", "translate(610,20)")
-        .append(() => Legend(color, {title: "Unemployment rate (%)", width: 260}));
-  
+
+    // svg.append("g")
+    //     .attr("transform", "translate(610,20)")
+    //     .append(() => Legend(color, {title: "Unemployment rate (%)", width: 260}));
+
     svg.append("g")
       .selectAll("path")
       .data(topojson.feature(us, us.objects.counties).features)
@@ -38,13 +58,19 @@ chart = {
         .attr("d", path)
       .append("title")
         .text(d => `${d.properties.name}, ${statemap.get(d.id.slice(0, 2)).properties.name}\n${valuemap.get(d.id)}%`);
-  
+
     svg.append("path")
         .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
         .attr("fill", "none")
         .attr("stroke", "white")
         .attr("stroke-linejoin", "round")
         .attr("d", path);
+
+  }, [ geodata, popdata ])
+
   
-    return svg.node();
-  }
+  return ( <svg></svg> )
+}
+
+
+export default Choropleth
